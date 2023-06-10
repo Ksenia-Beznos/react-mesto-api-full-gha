2f063b5071a8bcfa92eba5cd12241311875c2849
Kseniia <ksenia.beznos@yandex.ru>
2023-06-10 13:07:18 +0300

    18 commit

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,6 @@
-const jwt = require("jsonwebtoken");
-const Error401 = require("../errors/401");
-const JWT_DEV = require("../utils/jwtDev");
+const jwt = require('jsonwebtoken');
+const Error401 = require('../errors/401');
+const JWT_DEV = require('../utils/jwtDev');
 
 const { NODE_ENV } = process.env;
 const JWT_KEY = process.env.REACT_APP_JWT_SECRET;
@@ -9,12 +9,12 @@ const middlewareAuth = (req, res, next) => {
   try {
     const token = req.cookies.jwt;
     if (!token) {
-      throw new Error401("Токен отстутствует");
+      throw new Error401('Токен отстутствует');
     }
-    req.user = jwt.verify(token, NODE_ENV === "production" ? JWT_KEY : JWT_DEV);
+    req.user = jwt.verify(token, NODE_ENV === 'production' ? JWT_KEY : JWT_DEV);
     next();
   } catch {
-    const badToken = new Error401("Токен недействителен");
+    const badToken = new Error401('Токен недействителен');
     next(badToken);
   }
 };
